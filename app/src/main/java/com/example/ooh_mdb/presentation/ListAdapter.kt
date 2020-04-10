c93573685294e49cf234d01fe3e31cd8a03a2fb9
Serhii Kovtunenko <s.kovtunenko@eurospektras.lt>
2020-04-10 14:33:10 +0300

    refactor(ListAdapter): Extension rearrangements

--- app/src/main/java/com/example/ooh_mdb/presentation/ListAdapter.kt
+++ app/src/main/java/com/example/ooh_mdb/presentation/ListAdapter.kt
@@ -3,23 +3,18 @@ package com.example.ooh_mdb.presentation
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.domain.Movie
 import com.example.ooh_mdb.R
 import com.squareup.picasso.Picasso
-import kotlin.properties.Delegates
 
 
-class ListAdapter(//private val moviesList: List<Movie>,
-                  val itemClickListener: (View, Int) -> Unit
-//    val itemClickListener: OnItemClickListener
-)
+class ListAdapter(private val itemClickListener: (View, Int) -> Unit)
     : RecyclerView.Adapter<MovieViewHolder>() {
 
-    var list: List<Movie> by Delegates.observable(emptyList()) { _, _, _ -> notifyDataSetChanged() }
+    var list: List<Movie> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -45,24 +40,20 @@ class ListAdapter(//private val moviesList: List<Movie>,
 
 }
 
-//TODO: Should leave here or move to separate class?
 class MovieViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item, parent, false)) {
-    private var mTitleView: TextView? = null
-    private var mYearView: TextView? = null
-    private var mIconView: ImageView? = null
+    private var titleView: TextView?
+    private var yearView: TextView?
+    private var posterView: ImageView?
 
 
     init {
-        mTitleView = itemView.findViewById(R.id.list_title)
-        mYearView = itemView.findViewById(R.id.list_description)
-        mIconView = itemView.findViewById(R.id.list_icon)
+        titleView = itemView.findViewById(R.id.listItemTitle)
+        yearView = itemView.findViewById(R.id.listItemYear)
+        posterView = itemView.findViewById(R.id.listItemImage)
     }
 
-    fun bind(
-        movie: Movie//,
-//        itemClickListener: OnItemClickListener
-    ) {
+    fun bind( movie: Movie) {
 
         mTitleView?.text = movie.Title.orEmpty()
         mYearView?.text = movie.Year.orEmpty()
@@ -76,16 +67,5 @@ class MovieViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
 
 }
 
-//  Selected item recognition extension
-fun <T : RecyclerView.ViewHolder> T.onClick(event: (view: View, position: Int) -> Unit): T {
-    itemView.setOnClickListener {
-        event.invoke(it, getAdapterPosition())
-    }
-    return this
-}
-
-interface OnItemClickListener {
-    fun onItemClicked(movie: Movie)
-}
 
 
--- app/src/main/java/com/example/ooh_mdb/presentation/ViewExtension.kt
+++ app/src/main/java/com/example/ooh_mdb/presentation/ViewExtension.kt
@@ -2,6 +2,15 @@ package com.example.ooh_mdb.presentation
 
 import android.content.Context
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 val View.ctx: Context
     get() = context
+
+//  Selected item recognition extension
+fun <T : ViewHolder> T.onClick(event: (view: View, position: Int) -> Unit): T {
+    itemView.setOnClickListener {
+        event.invoke(it, adapterPosition)
+    }
+    return this
+}
